e23ccc21d281b971c6902379a9f433c19621e260
ChalkPE <chalkpe@gmail.com>
2016-03-15 19:15:05 +0900

    Small changes

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
+var express = require('express');
 
 var app = express();
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('port', process.env.PORT || '3000');
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(require('morgan')('dev'));
+app.use(require('cookie-parser')());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var routes = require('./routes/index');
 app.use('/', routes);
 
 app.use(function(req, res, next){
@@ -33,10 +31,5 @@ app.use(function(err, req, res, next){
     });
 });
 
-var debug = require('debug')('seize-online-rewrite:server');
-var http = require('http');
-
-app.set('port', process.env.PORT || '3000');
-
-var server = http.createServer(app);
+var server = require('http').createServer(app);
 server.listen(app.get('port'));
