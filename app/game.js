ce6c5941707c73133ba144cd8a45e06bff9e96ac
ChalkPE <chalkpe@gmail.com>
2016-03-17 13:48:46 +0900

    Updated common.js

--- app/game.js
+++ app/game.js
@@ -1,11 +1,27 @@
-function Game(world){
+var common = require('../public/js/common');
+var Direction = common.Direction;
+
+function Game(world, io){
     this.world = world;
+    this.io = io;
 }
 
 Game.prototype.getWorld = function(){
     return this.world;
 };
 
-Game.prototype.tick = function(){
-    
+Game.prototype.tick = function(buffer){
+    var self = this;
+    this.getWorld().forEach(function(field){
+        if(!field.isTerritory()) return;
+
+        if(field.isEnerge()){
+            field.meta += Math.floor((field.isAdvanced() ? 20 : 5) + Math.random() * 10);
+        }
+
+        var d = Math.floor(Math.random() * Direction.FOUR.length);
+        var sideField = field.getSideField(self.getWorld(), Direction.FOUR[d]);
+
+        //TODO: Implement this method
+    });
 };
--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,51 @@
+/*
+ * Copyright (C) 2015-2016  ChalkPE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 "use strict";
+
+var Direction = {
+    UP:    1,
+    RIGHT: 1 << 1,
+    DOWN:  1 << 2,
+    LEFT:  1 << 3,
+    CENTER_HORIZONTAL: 1 << 4,
+    CENTER_VERTICAL:   1 << 5,
+};
+
+Direction.NONE = 0;
+
+Direction.TOP    = Direction.UP;
+Direction.CENTER = Direction.CENTER_HORIZONTAL | Direction.CENTER_VERTICAL;
+Direction.BOTTOM = Direction.DOWN;
+
+Direction.NORTH = Direction.UP;
+Direction.EAST  = Direction.RIGHT;
+Direction.SOUTH = Direction.DOWN;
+Direction.WEST  = Direction.LEFT;
+
+Direction.NORTHEAST = Direction.NORTH | Direction.EAST;
+Direction.SOUTHEAST = Direction.SOUTH | Direction.EAST;
+Direction.SOUTHWEST = Direction.SOUTH | Direction.WEST;
+Direction.NORTHWEST = Direction.NORTH | Direction.WEST;
+
+Direction.FOUR  = [Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST];
+Direction.EIGHT = [Direction.NORTH, Direction.NORTHEAST, Direction.EAST, Direction.SOUTHEAST, Direction.SOUTH, Direction.SOUTHWEST, Direction.WEST, Direction.NORTHWEST];
+
+Object.freeze(Direction);
 
 var FieldType = {
     ENERGE:    1,
@@ -6,6 +53,8 @@ var FieldType = {
     ADVANCED:  1 << 2,
     BARRIER:   1 << 3
 };
+
+Object.freeze(FieldType);
 
 function Field(x, y, type, meta){
     this.x = x;
@@ -45,6 +94,22 @@ Field.prototype = {
 
     isBarrier: function(){
         return this.getType() & FieldType.BARRIER;
+    },
+
+    getSideField: function(world, direction){
+        var xx = 0;
+        var yy = 0;
+
+        if(direction & Direction.CENTER_HORIZONTAL) xx = 0;
+        if(direction & Direction.CENTER_VERTICAL) yy = 0;
+
+        if(direction & Direction.UP) yy = -1;
+        else if(direction & Direction.DOWN) yy = 1;
+
+        if(direction & Direction.LEFT) xx = -1;
+        else if(direction & Direction.RIGHT) xx = 1;
+
+        return world.getField(this.getX() + xx, this.getY() + yy);
     },
 
     toString: function(){
@@ -107,6 +172,7 @@ World.prototype = {
 };
 
 if(module) module.exports = {
+    'Direction': Direction,
     'FieldType': FieldType,
     'Field': Field,
     'World': World
